548e2e4344020f1a5f1187094d37948bdfad471f
oscarf <k3ther@gmail.com>
2016-09-29 16:33:39 +0200

    Removed unnecesary code

--- app/src/main/java/oscarf/test/MainActivity.java
+++ app/src/main/java/oscarf/test/MainActivity.java
@@ -1,14 +1,12 @@
 package oscarf.test;
 
 import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,16 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button myButton = (Button)findViewById(R.id.my_button);
-        final TextView myText = (TextView)findViewById(R.id.textView);
-
-        myButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v){
-                myText.setText(R.string.message);
-            }
-        });
-
+        Button loadButton = (Button)findViewById(R.id.loadButton);
         Button saveButton = (Button)findViewById(R.id.saveButton);
         final EditText textDisplay = (EditText)findViewById(R.id.textDisplay);
 
@@ -39,12 +28,10 @@ public class MainActivity extends AppCompatActivity {
                 // Save text in textDisplay
 
                 editor.putString("SAVED_TEXT",textDisplay.getText().toString());
-                editor.commit();
+                editor.apply();
 
             }
         });
-
-        Button loadButton = (Button)findViewById(R.id.loadButton);
 
         loadButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -56,10 +43,5 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-    }
-
-    public void goTo2Button(View view) {
-        Intent intent = new Intent(this, Activity2.class);
-        startActivity(intent);
     }
 }
