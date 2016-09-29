7b45e84a4b61d26b0fb9ebee653e81d9d2cdc0dd
oscarf <k3ther@gmail.com>
2016-09-29 15:57:27 +0200

    Added button to save and to load saved text

--- app/src/main/java/oscarf/test/MainActivity.java
+++ app/src/main/java/oscarf/test/MainActivity.java
@@ -1,10 +1,13 @@
 package oscarf.test;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,8 +22,37 @@ public class MainActivity extends AppCompatActivity {
 
         myButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View v){
                 myText.setText(R.string.message);
+            }
+        });
+
+        Button saveButton = (Button)findViewById(R.id.saveButton);
+        final EditText textDisplay = (EditText)findViewById(R.id.textDisplay);
+
+        final SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+        final SharedPreferences.Editor editor = sharedPref.edit();
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Save text in textDisplay
+
+                editor.putString("SAVED_TEXT",textDisplay.getText().toString());
+                editor.commit();
+
+            }
+        });
+
+        Button loadButton = (Button)findViewById(R.id.loadButton);
+
+        loadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Load saved text in textDisplay
+                String savedText = sharedPref.getString("SAVED_TEXT", "");
+                textDisplay.setText(savedText);
+
             }
         });
 
